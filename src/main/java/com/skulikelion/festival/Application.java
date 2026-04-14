0eb093561cd952aa92ca37b49ae46baf0c526883
Comeoksal <keumsiun0503@gmail.com>
2026-04-14 14:35:42 +0900

    :recycle: Refactor: Application 패키지 변경

--- src/main/java/com/skulikelion/festival/Application.java
+++ src/main/java/com/skulikelion/festival/Application.java
@@ -1,9 +1,12 @@
-package com.skulikelion;
+package com.skulikelion.festival;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan(basePackages = "com.skulikelion.festival.global.config.property")
 public class Application {
 
     public static void main(String[] args) {

d10a594a8cefcd1dbbec3fa073aa86c073116e26
Comeoksal <keumsiun0503@gmail.com>
2026-06-20 22:11:00 +0900

    :wrench: Settings: RestTemplate 사용을 위한 Bean 등록용 Configuration 클래스 추가

---
+++ src/main/java/com/skulikelion/festival/global/config/RestTemplateConfig.java
@@ -0,0 +1,17 @@
+/* 
+ * Copyright (c) SKU LIKELION 
+ */
+package com.skulikelion.festival.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+  @Bean
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
+}

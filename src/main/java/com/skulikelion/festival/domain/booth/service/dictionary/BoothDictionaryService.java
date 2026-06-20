d467f389860ec15d03601e2487a85f9ecd1723b0
Comeoksal <keumsiun0503@gmail.com>
2026-06-20 22:13:47 +0900

    :sparkles: Feat: 부스 번역 사전 관련 서비스 인터페이스 & 클래스 추가

---
+++ src/main/java/com/skulikelion/festival/domain/booth/service/dictionary/BoothDictionaryService.java
@@ -0,0 +1,10 @@
+/* 
+ * Copyright (c) SKU LIKELION 
+ */
+package com.skulikelion.festival.domain.booth.service.dictionary;
+
+public interface BoothDictionaryService {
+
+  // BoothMenuDictionary에 반영하는 메서드
+  // 트랜잭션 전파 수준 새로 생성하는 것으로
+}
---
+++ src/main/java/com/skulikelion/festival/domain/booth/service/dictionary/BoothDictionaryServiceImpl.java
@@ -0,0 +1,14 @@
+/* 
+ * Copyright (c) SKU LIKELION 
+ */
+package com.skulikelion.festival.domain.booth.service.dictionary;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class BoothDictionaryServiceImpl implements BoothDictionaryService {}

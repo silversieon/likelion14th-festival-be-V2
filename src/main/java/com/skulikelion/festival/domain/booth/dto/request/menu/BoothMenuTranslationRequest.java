489cf5a6eefa262fb3c03465a043cf4823504a29
Comeoksal <keumsiun0503@gmail.com>
2026-06-20 22:12:44 +0900

    :sparkles: Feat: 부스 메뉴 번역을 위한 dto 추가

---
+++ src/main/java/com/skulikelion/festival/domain/booth/dto/request/menu/BoothMenuTranslationRequest.java
@@ -0,0 +1,6 @@
+/* 
+ * Copyright (c) SKU LIKELION 
+ */
+package com.skulikelion.festival.domain.booth.dto.request.menu;
+
+public record BoothMenuTranslationRequest(String nameKo, String descriptionKo) {}
---
+++ src/main/java/com/skulikelion/festival/domain/booth/dto/response/menu/BoothMenuTranslationResponse.java
@@ -0,0 +1,12 @@
+/* 
+ * Copyright (c) SKU LIKELION 
+ */
+package com.skulikelion.festival.domain.booth.dto.response.menu;
+
+public record BoothMenuTranslationResponse(
+    String nameKo,
+    String nameEn,
+    String nameZh,
+    String descriptionKo,
+    String descriptionEn,
+    String descriptionZh) {}

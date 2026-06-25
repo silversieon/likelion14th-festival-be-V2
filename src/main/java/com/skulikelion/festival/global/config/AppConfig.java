6752d5ee06d9b06560a3dc7a15a428e4f299740e
Comeoksal <keumsiun0503@gmail.com>
2026-06-25 16:51:38 +0900

    :sparkles: Feat: ObjectMapper 빈 등록

---
+++ src/main/java/com/skulikelion/festival/global/config/AppConfig.java
@@ -0,0 +1,18 @@
+/* 
+ * Copyright (c) SKU LIKELION 
+ */
+package com.skulikelion.festival.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
+public class AppConfig {
+
+  @Bean
+  public ObjectMapper objectMapper() {
+    return new ObjectMapper();
+  }
+}

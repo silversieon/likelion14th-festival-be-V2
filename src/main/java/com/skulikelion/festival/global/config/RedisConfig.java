fd03e2c44f748e71165ba7eda5189a6fbf4a5ea3
Comeoksal <keumsiun0503@gmail.com>
2026-07-28 20:22:28 +0900

    :recycle: Refactor: redisConnectionFactory 생성을 SpringBoot에게 맡겨 DI 활용, 팩토리를 주입 받는 식으로 변경

--- src/main/java/com/skulikelion/festival/global/config/RedisConfig.java
+++ src/main/java/com/skulikelion/festival/global/config/RedisConfig.java
@@ -3,40 +3,19 @@
  */
 package com.skulikelion.festival.global.config;
 
-import org.springframework.boot.data.redis.autoconfigure.DataRedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import lombok.RequiredArgsConstructor;
-
 @Configuration
-@RequiredArgsConstructor
-@EnableRedisRepositories
 public class RedisConfig {
 
-  private final DataRedisProperties dataRedisProperties;
-
   @Bean
-  public RedisConnectionFactory redisConnectionFactory() {
-    RedisStandaloneConfiguration redisConfig =
-        new RedisStandaloneConfiguration(
-            dataRedisProperties.getHost(), dataRedisProperties.getPort());
-    if (dataRedisProperties.getPassword() != null && !dataRedisProperties.getPassword().isEmpty()) {
-      redisConfig.setPassword(dataRedisProperties.getPassword());
-    }
-    return new LettuceConnectionFactory(redisConfig);
-  }
-
-  @Bean
-  public RedisTemplate<String, String> redisTemplate() {
+  public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
     RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-    redisTemplate.setConnectionFactory(redisConnectionFactory());
+    redisTemplate.setConnectionFactory(connectionFactory);
     redisTemplate.setKeySerializer(new StringRedisSerializer());
     redisTemplate.setValueSerializer(new StringRedisSerializer());
     return redisTemplate;

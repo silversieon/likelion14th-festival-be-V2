7992f7a7d5d34b9374f0881e378d5f1e45b3be9d
Ksieon <keumsiun0503@gmail.com>
2026-09-03 11:40:06 +0900

    :wrench: Settings: compose 설정 변경

--- start.bat
+++ start.bat
@@ -6,10 +6,10 @@ echo Starting docker...
 cd docker/local
 docker compose ^
   --compatibility ^
-  -f docker-compose-local.yml ^
   -f docker-compose-app-01.yml ^
   -f docker-compose-app-02.yml ^
   -f docker-compose-app-03.yml ^
+   -f docker-compose-local.yml ^
   up -d --build
 
 echo Done!
